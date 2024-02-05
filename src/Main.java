c59abede711d34d4324f59f5b2ac7ebe56e8e013
LuisMiguelB <luibenmor@gmail.com>
2024-02-05 13:47:15 +0100

    Class + Tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Racional f1 = new Racional(1, 2);
+        Racional f2 = new Racional(2, 3);
+
+        Racional f3 = f1.suma(f2);
+        Racional f4 = f1.resta(f2);
+        Racional f5 = f1.multiplicacion(f2);
+        Racional f6 = f1.division(f2);
+
+        System.out.println(f3.toString());
+        System.out.println(f4.toString());
+        System.out.println(f5.toString());
+        System.out.println(f6.toString());
+
     }
 }
--- src/Racional.java
+++ src/Racional.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Racional {
+public class Racional {
+
+    private int numerador, denominador;
+
+    public Racional() {
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    public Racional(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public Racional suma(Racional r) {
+        int num = this.numerador * r.denominador + r.numerador * this.denominador;
+        int den = this.denominador * r.denominador;
+        return new Racional(num, den);
+    }
+
+    public Racional resta(Racional r) {
+        int num = this.numerador * r.denominador - r.numerador * this.denominador;
+        int den = this.denominador * r.denominador;
+        return new Racional(num, den);
+    }
+
+    public Racional multiplicacion(Racional r) {
+        int num = this.numerador * r.numerador;
+        int den = this.denominador * r.denominador;
+        return new Racional(num, den);
+    }
+
+    public Racional division(Racional r) {
+        int num = this.numerador * r.denominador;
+        int den = this.denominador * r.numerador;
+        return new Racional(num, den);
+    }
+
+    public String toString() {
+        return this.numerador + "/" + this.denominador;
+    }
+
 }
